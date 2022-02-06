04b37ff942605729e3eeb973dcb51df34419a9fc
devtopher <christopher.david.mccoy@gmail.com>
2022-02-05 22:08:25 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+/**
+Challenge: 
+
+- Start building out the BoredBot Skeleton however you'd like. 
+    That will include:
+    - A title for the app ("BoredBot" might be a good start 😉)
+    - A placeholder element that will be populated with the random 
+      idea we get from the API
+    - A button to click for triggering the GET request to the Bored API. 
+      (Don't worry about implementing the button quite yet)
+*/
+let btn = document.getElementById("btn")
+btn.addEventListener("click", fetchBoredAPI)
+function fetchBoredAPI(){
+fetch("https://apis.scrimba.com/bored/api/activity")
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById("activity").textContent = `
+    ${data.activity}
+    `
+  })}
